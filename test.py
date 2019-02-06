a6fa2bdb2a14e3f25657f93fbf6a11c2e9e46748
sittipongkongkaew123 <shadowzr12@gmail.com>
2019-02-06 14:59:03 +0700

    edit temperature file

--- test.py
+++ test.py
@@ -10,9 +10,15 @@ def check_temp():
 if __name__ == '__main__':
     t = input("Enter your temperture :")
     while t !='q':
+        if t.isnumeric()
+            t = int(t)
+            print(check_temp(t))
+        else:
+            print("You must enter number of temperature:")
+
         t = int(t)
         check_temp(t)
-        t = input("Enter your temperture :")
+        t = input("Enter mew temperture :")
     sys.exit(0)
 
 
